e1d6b0626b4009c5146bfd833ae594974b178d48
Roman Barakhvostov <rbarakhvostov@gmail.com>
2019-06-17 18:04:10 +0300

    feat: modify pen tool

--- piskel-clone/src/utilities/drawWithPen.js
+++ piskel-clone/src/utilities/drawWithPen.js
@@ -6,23 +6,24 @@ export default function drawWithPen() {
   const unitsNumber = 64;
   const pixelSize = canvas.width / unitsNumber;
   let paint;
-  function draw(x, y) {
+  function draw(x, y, button) {
     const primaryColor = document.querySelector('.primary-color');
-    // context.rect(x, y, pixelSize, pixelSize);
-    context.fillStyle = primaryColor.value;
+    const secondaryColor = document.querySelector('.secondary-color');
+    if (button === 1) context.fillStyle = primaryColor.value; // 1 - left button
+    if (button === 3) context.fillStyle = secondaryColor.value; // 3 - right button
     context.fillRect(x, y, pixelSize, pixelSize);
   }
   function mousedownHandler(e) {
     const mouseX = Math.floor((e.pageX - canvas.offsetLeft) / pixelSize) * pixelSize;
     const mouseY = Math.floor((e.pageY - canvas.offsetTop) / pixelSize) * pixelSize;
     paint = true;
-    draw(mouseX, mouseY);
+    draw(mouseX, mouseY, e.which);
   }
   function mousemoveHandler(e) {
     if (paint) {
       const mouseX = Math.floor((e.pageX - canvas.offsetLeft) / pixelSize) * pixelSize;
       const mouseY = Math.floor((e.pageY - canvas.offsetTop) / pixelSize) * pixelSize;
-      draw(mouseX, mouseY);
+      draw(mouseX, mouseY, e.which);
     }
   }
   function mouseupHandler() {
@@ -36,4 +37,5 @@ export default function drawWithPen() {
   canvas.addEventListener('mousemove', mousemoveHandler);
   canvas.addEventListener('mouseup', mouseupHandler);
   canvas.addEventListener('mouseleave', mouseleaveHandler);
+  canvas.addEventListener('contextmenu', e => e.preventDefault());
 }
