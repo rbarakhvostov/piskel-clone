3160f68f2426742fca9912d36a96a294ebb36d2b
Roman Barakhvostov <rbarakhvostov@gmail.com>
2019-06-21 00:20:11 +0300

    fix: change the pen size selection function

--- piskel-clone/src/components/penSizes/renderPenSizes.js
+++ piskel-clone/src/components/penSizes/renderPenSizes.js
@@ -3,6 +3,6 @@ import selectPenSize from '../../utilities/selectPenSize';
 
 export default function renderPenSizes() {
   const toolbar = document.querySelector('.toolbar');
-  toolbar.insertAdjacentHTML('beforeEnd', '<form class="pen-sizes"><input class="pen-size" value="128" type="radio" name="pen-size" id="pen-size-1"><label class="pen-size-label pen-size-1-label" for="pen-size-1"><div></div></label><input class="pen-size" value="64" type="radio" name="pen-size" id="pen-size-2" checked><label class="pen-size-label pen-size-2-label" for="pen-size-2"><div></div></label><input class="pen-size" value="32" type="radio" name="pen-size" id="pen-size-3"><label class="pen-size-label pen-size-3-label" for="pen-size-3"><div></div></label></form>');
+  toolbar.insertAdjacentHTML('beforeEnd', '<form class="pen-sizes"><input class="pen-size" value="128" type="radio" name="pen-size" id="pen-size-1"><label class="pen-size-label pen-size-label-1" for="pen-size-1"><div></div></label><input class="pen-size" value="64" type="radio" name="pen-size" id="pen-size-2" checked><label class="pen-size-label pen-size-label-2 selected-size" for="pen-size-2"><div></div></label><input class="pen-size" value="32" type="radio" name="pen-size" id="pen-size-3"><label class="pen-size-label pen-size-label-3" for="pen-size-3"><div></div></label></form>');
   document.querySelector('.pen-sizes').addEventListener('click', selectPenSize);
 }
--- piskel-clone/src/utilities/selectPenSize.js
+++ piskel-clone/src/utilities/selectPenSize.js
@@ -1,12 +1,21 @@
 export default function selectPenSize(e) {
-  const { target } = e;
-  const sizes = this.querySelectorAll('label');
-  if (target.tagName === 'DIV' || target.tagName === 'LABEL') {
-    sizes.forEach((el) => {
-      const copyEl = el;
-      copyEl.style.backgroundColor = '';
-    });
+  let { target } = e;
+  // const sizes = this.querySelectorAll('label');
+  // if (target.tagName === 'DIV' || target.tagName === 'LABEL') {
+  //   sizes.forEach((el) => {
+  //     const copyEl = el;
+  //     copyEl.style.backgroundColor = 'transparent';
+  //   });
+  // }
+  // if (target.tagName === 'LABEL') target.style.backgroundColor = '#0cafb7';
+  // if (target.tagName === 'DIV') target.parentNode.style.backgroundColor = '#0cafb7';
+  while (target !== this) {
+    if (target.tagName === 'LABEL') {
+      const selectedTool = document.querySelector('.selected-size');
+      selectedTool.classList.remove('selected-size');
+      target.classList.add('selected-size');
+      return;
+    }
+    target = target.parentNode;
   }
-  if (target.tagName === 'LABEL') target.style.backgroundColor = '#0cafb7';
-  if (target.tagName === 'DIV') target.parentNode.style.backgroundColor = '#0cafb7';
 }
