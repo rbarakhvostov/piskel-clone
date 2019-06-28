8db29deec88c75f28e1a71067daa2e1d8c2e5172
Roman Barakhvostov <rbarakhvostov@gmail.com>
2019-06-28 18:54:04 +0300

    feat: create landing page

--- piskel-clone/src/components/header/renderHeader.js
+++ piskel-clone/src/components/header/renderHeader.js
@@ -2,5 +2,5 @@ import './header.css';
 
 export default function renderHeader() {
   const body = document.querySelector('body');
-  body.insertAdjacentHTML('afterBegin', '<header class="header"><div class="header-container"><h1 class="title"><a href="#">Piskel Clone</a></h1></div></header>');
+  body.insertAdjacentHTML('afterBegin', '<header class="header"><h1 class="title">Piskel Clone</h1></header>');
 }
