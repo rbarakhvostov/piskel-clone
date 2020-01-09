a84e958e5de646b8695d5e7716ed6a97cf9d5e78
rbarakhvostov <rbarakhvostov@gmail.com>
2020-01-10 01:59:18 +0300

    feat: add favicon

--- piskel-clone/src/app.js
+++ piskel-clone/src/app.js
@@ -4,6 +4,9 @@ import startDrawWithPen from './utilities/startDrawWithPen';
 import startShowInfo from './utilities/startShowInfo';
 import assignShortcuts from './utilities/assignShortcuts';
 
+import '../styles/reset.css';
+import './app.css';
+
 function start() {
   renderHeader();
   renderMain();
--- piskel-clone/webpack.config.js
+++ piskel-clone/webpack.config.js
@@ -24,7 +24,8 @@ module.exports = {
     ],
   },
   plugins: [new HtmlWebpackPlugin({
-    template: 'index.html',
+    template: 'public/index.html',
+    favicon: 'public/favicon.png',
   })],
   devServer: {
     port: 9000,
