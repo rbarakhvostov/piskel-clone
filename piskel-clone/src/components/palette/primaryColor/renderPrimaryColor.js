94c307f4739dc4047216b119077a0720989f0df1
rbarakhvostov <rbarakhvostov@gmail.com>
2020-01-10 18:59:50 +0300

    refactor: change primary and secondary colors

--- piskel-clone/src/components/palette/primaryColor/renderPrimaryColor.js
+++ piskel-clone/src/components/palette/primaryColor/renderPrimaryColor.js
@@ -3,5 +3,5 @@ import './primaryColor.css';
 export default function renderPrimaryColor() {
   const palette = document.querySelector('.palette');
   palette.insertAdjacentHTML('beforeEnd',
-    '<input type="color" class="primary-color" title="primary color [left button]">');
+    '<input type="color" class="primary-color" value="#f2e494" title="primary color [left button]">');
 }
--- piskel-clone/src/components/palette/secondaryColor/renderSecondaryColor.js
+++ piskel-clone/src/components/palette/secondaryColor/renderSecondaryColor.js
@@ -3,5 +3,5 @@ import './secondaryColor.css';
 export default function renderSecondaryColor() {
   const palette = document.querySelector('.palette');
   palette.insertAdjacentHTML('beforeEnd',
-    '<input type="color" class="secondary-color" title="secondary color [right button]">');
+    '<input type="color" class="secondary-color" value="#88c4f2" title="secondary color [right button]">');
 }
